de144d3c7505c2c8957a78fdbd0cff7b3a34575d
Connor <179545997+thimbleforth@users.noreply.github.com>
2026-01-26 16:41:06 -0700

    Increase maximum heap size in server-start.sh to improve performance

--- server-start.sh
+++ server-start.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
 set -euo pipefail
-java -Xms2G -Xmx4G -XX:AOTCache=/hytale/Server/HytaleServer.aot --enable-native-access=ALL-UNNAMED -jar /hytale/Server/HytaleServer.jar --assets /hytale/Assets.zip
+java -Xms2G -Xmx8G -XX:AOTCache=/hytale/Server/HytaleServer.aot --enable-native-access=ALL-UNNAMED -jar /hytale/Server/HytaleServer.jar --assets /hytale/Assets.zip
